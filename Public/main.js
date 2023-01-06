d587de54cc13ee17ba5dc285ce03bf548e21158a
Yuran Simao <yuransimao7@gmail.com>
2023-01-06 20:48:09 +0100

    update

--- Public/main.js
+++ Public/main.js
@@ -2,6 +2,6 @@ const btn = document.querySelector('input')
 const body = document.querySelector('body')
 btn.onclick = e => {
 
-
+    body.classList.toggle('active') 
     console.log("excutando")
 }
